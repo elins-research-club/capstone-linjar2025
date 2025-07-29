b068336095453946782326946609617a9e185760
imki <146008470+imkinasyiatull@users.noreply.github.com>
2025-07-29 17:07:20 +0700

    Add files via upload

---
+++ hardprog/bodytemp/bodytemp.cpp
@@ -0,0 +1,8 @@
+#include "bodytemp.h"
+#include "Arduino.h"
+
+float readBodytemp(int pin_gy906baa){
+    
+
+    return bodytemp;
+}
---
+++ hardprog/bodytemp/bodytemp.h
@@ -0,0 +1,8 @@
+#ifndef BODYTEMP_H
+#define BODYTEMP_H
+
+#include "Arduino.h"
+
+float readBodytemp(int pin_gy906baa);
+
+#endif
