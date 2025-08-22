f4ee224d19ed976e705a4930bbe90fcab8b6fdcb
Imki <imkinasyia@gmail.com>
2025-08-22 16:19:56 +0700

    Add main code python

--- jetson_send_to_software.py
+++ jetson_send_to_software.py
@@ -1,3 +1,8 @@
 from jetson_read_serial import *
+from plan_laptop.jetson_receive_string import msg
+import json
+
+wingspan = float(msg)
 
 # kirim sensor_value ke software 
+# urutan sensor: "tinggi;berat;suhu;oksigen;heartrate;wingspan"
